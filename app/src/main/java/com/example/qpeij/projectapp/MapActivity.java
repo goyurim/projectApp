e14b329e6340e1b0ca28e79af9c8a6a9d3fe6b2a
chaeyoon <yoonchae125>
2018-05-27 23:15:45 +0900

    야야~

--- app/src/main/java/com/example/qpeij/projectapp/MapActivity.java
+++ app/src/main/java/com/example/qpeij/projectapp/MapActivity.java
@@ -2,6 +2,9 @@ package com.example.qpeij.projectapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    public void onClickedLocalBtn(View view) {
+        PopupMenu pop = new PopupMenu(this,view);
+        pop.getMenuInflater().inflate(R.menu.menu,pop.getMenu());
+
+        pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                //인텐트
+                return true;
+            }
+        });
+        pop.show();
     }
 }
