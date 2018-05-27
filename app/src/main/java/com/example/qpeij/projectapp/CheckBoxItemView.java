b2273c460e528cd0a2ffa3b1327b044cb74989b8
qpeij <goyurim>
2018-05-28 00:50:39 +0900

    체크리스트 기본 UI

---
+++ app/src/main/java/com/example/qpeij/projectapp/CheckBoxItemView.java
@@ -0,0 +1,37 @@
+package com.example.qpeij.projectapp;
+
+/**
+ * Created by qpeij on 2018-05-28.
+ */
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class CheckBoxItemView extends LinearLayout{
+    TextView checkboxItem;
+    public CheckBoxItemView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CheckBoxItemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.checkbox_item, this,true);
+        checkboxItem = (TextView)findViewById(R.id.checkboxContent);
+    }
+
+    public void setContent(String checkboxItem) {
+        this.checkboxItem.setText(checkboxItem);
+    }
+}
+
