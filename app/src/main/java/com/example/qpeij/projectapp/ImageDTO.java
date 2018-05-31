54a9f5fe2b93f01cc77f58b940a952bf16d31206
chaeyoon <yoonchae125>
2018-05-31 23:53:05 +0900

    ㄹ

---
+++ app/src/main/java/com/example/qpeij/projectapp/ImageDTO.java
@@ -0,0 +1,7 @@
+package com.example.qpeij.projectapp;
+
+public class ImageDTO {
+    public String local;
+    public String mainImage;
+    public String image;
+}
--- app/src/main/java/com/example/qpeij/projectapp/PhotoAddActivity.java
+++ app/src/main/java/com/example/qpeij/projectapp/PhotoAddActivity.java
@@ -31,6 +31,8 @@ import android.provider.*;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -38,9 +40,12 @@ import com.google.firebase.storage.UploadTask;
 public class PhotoAddActivity extends AppCompatActivity {
     Button photoAdd;
     GridView photoListView;
+    String LocalName;
     PhotoAdapter photoAdapter;
     private final int PICK_IMAGE = 1;
     FirebaseStorage storage;
+    FirebaseDatabase database;
+    private DatabaseReference mDatabase;
     private ProgressDialog detectionProgressDialog;
 
     @Override
@@ -48,6 +53,11 @@ public class PhotoAddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_add);
         storage=FirebaseStorage.getInstance();
+        database=FirebaseDatabase.getInstance();
+        mDatabase=database.getReference();
+        
+        //지역명 받아올것
+        LocalName="경기도";
 
         //권한
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -55,6 +65,7 @@ public class PhotoAddActivity extends AppCompatActivity {
         }
 
         photoAdd = (Button)findViewById(R.id.photoAdd);
+
         photoListView=(GridView)findViewById(R.id.photoListView);
         photoAdapter = new PhotoAdapter();
         photoListView.setAdapter(photoAdapter);
@@ -69,10 +80,6 @@ public class PhotoAddActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(photoAdapter.getCount()<=10) {
-                   /*Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                    gallIntent.setType("image/*");
-                    startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
-*/
                      Intent intent = new Intent(Intent.ACTION_PICK);
                     intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
 
@@ -88,31 +95,30 @@ public class PhotoAddActivity extends AppCompatActivity {
 
         //리스트에 항목 선택하면 이벤트 처리
         photoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                //팝업창띄우기
-                PopupMenu pop = new PopupMenu(getApplicationContext(),view);
-                pop.getMenuInflater().inflate(R.menu.photo_menu,pop.getMenu());
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+            //팝업창띄우기
+            PopupMenu pop = new PopupMenu(getApplicationContext(),view);
+            pop.getMenuInflater().inflate(R.menu.photo_menu,pop.getMenu());
 
-                pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
+            pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
 
-                        int id = menuItem.getItemId();
-                        if(id==R.id.setMain){
-                            //대표사진
-                        }
-                        else if(id==R.id.deletePhoto) {
-                            //삭제
-                        }
-                        return PhotoAddActivity.super.onOptionsItemSelected(menuItem);
+                    int id = menuItem.getItemId();
+                    if(id==R.id.setMain){
+                        //대표사진
                     }
-                });
-                pop.show();
-            }
-        });
-    }
-
+                    else if(id==R.id.deletePhoto) {
+                        //삭제
+                    }
+                    return PhotoAddActivity.super.onOptionsItemSelected(menuItem);
+                }
+            });
+            pop.show();
+        }
+    });
+}
     class PhotoAdapter extends BaseAdapter{
         ArrayList<PhotoItem> items = new ArrayList<PhotoItem>();
         @Override
@@ -150,61 +156,14 @@ public class PhotoAddActivity extends AppCompatActivity {
 
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                //ImageView imageView = (ImageView) findViewById(R.id.imageView1);
-                //imageView.setImageBitmap(bitmap);
-                //photoAdapter.addItem(new PhotoItem(bitmap.getGenerationId()));
                 photoAdapter.addItem(new PhotoItem(bitmap));
                 photoAdapter.notifyDataSetChanged();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            StorageReference storageRef = storage.getReference();
 
-
-            Uri file = Uri.fromFile(new File(getPath(uri)));
-            StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
-            UploadTask uploadTask = riversRef.putFile(file);
-
-// Register observers to listen for when the download is done or if it fails
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    // Handle unsuccessful uploads
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                    // ...
-                }
-            });
+            upload(getPath(uri));
         }
-       /* if(requestCode==PICK_IMAGE){
-            //사진가져오기
-            //사진경로
-           // System.out.println(getPath(data.getData()));
-
-            StorageReference storageRef = storage.getReference();
-
-
-            Uri file = Uri.fromFile(new File(getPath(data.getData())));
-            StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
-            UploadTask uploadTask = riversRef.putFile(file);
-
-// Register observers to listen for when the download is done or if it fails
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    // Handle unsuccessful uploads
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                    // ...
-                }
-            });
-        }*/
     }
     public String getPath(Uri uri){
         String [] proj = {MediaStore.Images.Media.DATA};
@@ -216,5 +175,30 @@ public class PhotoAddActivity extends AppCompatActivity {
         cursor.moveToFirst();
 
         return cursor.getString(index);
+    }
+    private void upload(String uri){
+        StorageReference storageRef = storage.getReference();
+
+        Uri file = Uri.fromFile(new File(uri));
+        StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = riversRef.putFile(file);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                ImageDTO imageDTO = new ImageDTO();
+                imageDTO.local=LocalName;
+                imageDTO.image =(downloadUrl.toString());
+
+                database.getReference().child("MapDB").push().setValue(imageDTO);
+            }
+        });
     }
 }
