a622a637476f6c4efcdf99a6fd2f37719d7fa26c
chaeyoon <yoonchae125>
2018-06-01 18:00:42 +0900

    롸

--- app/src/main/java/com/example/qpeij/projectapp/CheckBoxList2Activity.java
+++ app/src/main/java/com/example/qpeij/projectapp/CheckBoxList2Activity.java
@@ -2,17 +2,21 @@ package com.example.qpeij.projectapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -31,12 +35,15 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CheckBoxList2Activity extends AppCompatActivity {
     EditText ed_cbItem; //체크박스 내용 입력받는 것
     ListView listView;// 리스트 뷰
     TextView checklistTitle;//체크박스 리스트 이름
-    int id;//아이디 값
+    //int id;//아이디 값
+    int position;
+    private List<String> uidLists = new ArrayList<>();
     String title;
     CheckBoxListAdapter adapter;
     CheckBoxItem checkBoxItem;
@@ -56,7 +63,7 @@ public class CheckBoxList2Activity extends AppCompatActivity {
         //TODOList 화면에서 받음
         Intent intent = getIntent();
         title = intent.getStringExtra("title"); //리스트 제목
-        id = intent.getIntExtra("id",0);//리스트 아이디값
+        //id = intent.getIntExtra("id",0);//리스트 아이디값
         listView = (ListView) findViewById(R.id.checkboxlistview);
         ed_cbItem=(EditText)findViewById(R.id.ed_checkboxItem) ;
         storage=FirebaseStorage.getInstance();
@@ -89,13 +96,79 @@ public class CheckBoxList2Activity extends AppCompatActivity {
 
             }
         });
+
+        //길게누르면 -안됨 왠지 모름**
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("Log","dsf");
+                position = i;
+                showMessage();
+                return true;
+
+            }
+        });
     }
-//추가하기버튼
+    private void showMessage(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("안내");
+        builder.setMessage("삭제하시겠습니까?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        Log.d("Log","longClick");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //예 누르면
+                delete_content(position);
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //아니오 누르면
+            }
+        });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+    }
+    private void delete_content(final int position){
+        //storage.getReference().child("MapDB").child(uidLists.get(position)).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+        // @Override
+        // public void onSuccess(Void aVoid) {
+
+        database.getReference().child("MapDB").child(uidLists.get(position)).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+                Toast.makeText(getApplicationContext(), "삭제가 완료 되었습니다.", Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "삭제 실패", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // }
+        // }).addOnFailureListener(new OnFailureListener() {
+        //    @Override
+        //   public void onFailure(@NonNull Exception e) {
+        //
+
+        //   }
+        // });
+
+    }
+
+    //추가하기버튼
     public void checkBoxCreateButton(View view) {
 
         adapter.addItem(new CheckBoxItem(ed_cbItem.getText().toString()));
         upload();
+        adapter.notifyDataSetChanged();
+        ed_cbItem.setText("");
     }
+
 
     class CheckBoxListAdapter extends BaseAdapter {
         ArrayList<CheckBoxItem> items = new ArrayList<CheckBoxItem>();
