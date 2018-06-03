1b8d7e0a0df6242e543821119a9339f92dac8cc2
Haeeul <etoilune00@gmail.com>
2018-06-03 22:47:20 +0900

    ,

--- app/src/main/java/com/example/qpeij/projectapp/SiteActivity.java
+++ app/src/main/java/com/example/qpeij/projectapp/SiteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
@@ -21,52 +22,58 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.nhn.android.maps.NMapView;
 
 public class SiteActivity extends AppCompatActivity {
 
-    private String key;
-    private double latitude;
-    private double longitude;
-    private String site;
+    //지도
+    private NMapView mMapView;// 지도 화면 View
+    private final String CLIENT_ID = "yMxM1_6BoFVYF8jXenYu";// 애플리케이션 클라이언트 아이디 값
+
+     String key;
+     double latitude;
+     double longitude;
+     String site;
     FirebaseStorage storage;
     FirebaseDatabase database;
     SiteDTO siteDTO;
     TextView textView;
     ImageView siteImage1, siteImage2;
-    WebView mapView; //지도여기에 넣어라 천명희
+    MapViewFragmentNaver mapViewFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_site);
 
-
-        //key값 받아오기
-        Intent intent=getIntent();
-        key = intent.getStringExtra("key");
-
-        mapView=(WebView) findViewById(R.id.mapVIew);
         textView=(TextView)findViewById(R.id.textView);
         siteImage1=(ImageView)findViewById(R.id.siteImage1);
         siteImage2=(ImageView)findViewById(R.id.siteImage2);
 
+        Intent intent=getIntent();
+        key=intent.getStringExtra("key");
         //db
         storage=FirebaseStorage.getInstance();
         database=FirebaseDatabase.getInstance();
 
-        //database.getReference().child("SiteDB").child(key).addValueEventListener(new ValueEventListener() {
+        //database.getReference().child("SiteDB").chil(key).addValueEventListener(new ValueEventListener() {
         database.getReference().child("SiteDB").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                     siteDTO = snapshot.getValue(SiteDTO.class);
+
                     if(key.equals(siteDTO.local)){
                         textView.setText(siteDTO.site);
                         latitude=siteDTO.latitude;
                         longitude=siteDTO.longtitude;
                         Glide.with(getApplicationContext()).load(Uri.parse(siteDTO.siteImage1)).into(siteImage1);
                         Glide.with(getApplicationContext()).load(Uri.parse(siteDTO.siteImage2)).into(siteImage2);
-                       // siteImage1.setImageURI(siteDTO.siteImage1);
+                        //key값 받아오기
+                        Log.d("log","보낸"+latitude);
+                        Log.d("log","보낸"+longitude);
+                        // siteImage1.setImageURI(siteDTO.siteImage1);
                        // siteImage2.setImageURI(siteDTO.siteImage2);
                     }
                 }
@@ -76,52 +83,8 @@ public class SiteActivity extends AppCompatActivity {
 
             }
         });
-      /*  String url;
-        switch (key){
-            case "서울":
-                url = "http://naver.me/5r6ygG2I";
-                webViewContect(url);
-                break;
-            case "춘천":
-                url = "http://naver.me/5lYU1cKu";
-                webViewContect(url);
-                break;
-            case "강릉":
-                url = "http://naver.me/GcMxatOv";
-                webViewContect(url);
-                break;
-            case "대전":
-                url = "http://naver.me/Fvh4lRT4";
-                webViewContect(url);
-                break;
-            case "대구":
-                url = "http://naver.me/x4tEgeTL";
-                webViewContect(url);
-                break;
-            case "부산":
-                url = "http://naver.me/FiHWyYWJ";
-                webViewContect(url);
-                break;
-            case "전주":
-                url = "http://naver.me/5inQGEG7";
-                webViewContect(url);
-                break;
-                default:
-                    url = "http://naver.me/xYgdxNlu";
-                    webViewContect(url);
-                    break;
-        }*/
+        mapViewFragment = (MapViewFragmentNaver) getSupportFragmentManager().findFragmentById(R.id.mapView);
 
     }
-/*
-    public void webViewContect(String url){
-        String loadUrl = url;
-        WebSettings webSettings = mapView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
 
-        mapView.setWebViewClient(new WebViewClient());
-        mapView.loadUrl(loadUrl);
-
-    }
-      */
 }
